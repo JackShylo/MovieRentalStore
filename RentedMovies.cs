c90532a1babf21eb97c8df9a2de27eb79d3b18ab
Jack Kelly <xJaquar7@gmail.com>
2020-06-01 07:56:10 +1200

    Add project files.

---
+++ RentedMovies.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalStore
+{
+    class RentedMovies
+    {
+        Database rentedMoviesDatabase = new Database();
+
+        public DataView ReadEntries(string selectFields, string tableName, string whereClause)
+        {
+
+            return rentedMoviesDatabase.ReadEntries(selectFields, tableName, whereClause);
+        }
+
+        public bool IssueMovie(string[] insertArr)
+        {
+            string tableName = "RentedMovies";
+            // parameterDefine, parameterAssign & insertArr need to be the same length.
+            string parameterDefine = "@MovieIDFK, @CustIDFK, @DateRented, @DateReturned";
+            string[] parameterAssign = { "@MovieIDFK", "@CustIDFK", "@DateRented", "@DateReturned" };
+            return rentedMoviesDatabase.CreateEntry(tableName, parameterDefine, parameterAssign, insertArr);
+        }
+
+        public bool ReturnMovie(string[] updateArr)
+        {
+            string tableName = "RentedMovies";
+            string setFields = "DateReturned=@DateReturned";
+            string whereClause = "WHERE RMID=@RMID";
+            string[] parameterAssign = { "@DateReturned", "@RMID" };
+            return rentedMoviesDatabase.UpdateEntry(tableName, setFields, whereClause, parameterAssign, updateArr);
+        }
+    }
+}
